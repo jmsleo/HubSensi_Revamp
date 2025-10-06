0bc196dd2f1a13871c3bea60e49ed86617fcc38d
Thomas <thomas.sugiarto04@gmail.com>
2025-10-06 15:50:57 +0700

    [HUBSENSI REVAMP][PUBLISH] Fix worker deployment #Thom

--- celery_worker.py
+++ celery_worker.py
@@ -1,11 +1,20 @@
+# HubSensi_(GitHub Production)/celery_worker.py
 import os
 from celery import Celery
 from dotenv import load_dotenv
 
+# Muat environment variables terlebih dahulu
 load_dotenv()
 
+# Buat instance Celery dan beritahu di mana letak file tasks.py
 celery = Celery(
-    "HubSensi-Celery", # Anda bisa beri nama apa saja
+    "HubSensi",
+    broker=os.environ.get('CELERY_BROKER_URL'),
     backend=os.environ.get('CELERY_RESULT_BACKEND'),
-    broker=os.environ.get('CELERY_BROKER_URL')
+    include=['tasks']  # <-- Baris ini adalah kuncinya
+)
+
+# Konfigurasi tambahan (opsional tapi bagus untuk dimiliki)
+celery.conf.update(
+    task_track_started=True,
 )
